6855c4ecacc3487e4cf7ff76d8f81f7cbd047a77
Randy <blue.ender.ty@gmail.com>
2025-01-22 12:40:21 -0700

    Add files via upload
    Compressed code and replaced the true statement for the while loop

---
+++ showbreak.py
@@ -0,0 +1,25 @@
+### January 22, 2025
+### Keenan Young COMSC 175
+### ShowBreak.py
+
+"""
+unwanted text
+Print statement %
+"""
+
+from math import factorial
+
+while __name__:
+   N = input('Enter a nonnegative integer: ')
+   try:
+      fatN = factorial(int(N))
+      if int(N)>=0:
+         break
+   except ValueError:
+      print("ValueError")
+      print('N must be non-negative integer.')
+      continue
+   
+   
+print('\n\n%1d! = %1d' % (int(N),fatN))
+
